589da4a5e8e0b4a7effa6510602e2c776176f863
Nijam26 <nijam93.ctg@gmail.com>
2025-01-24 13:31:14 +0600

    NewDesktopLogin

--- KokilApp/obj/Release/net8.0/KokilApp.AssemblyInfo.cs
+++ KokilApp/obj/Release/net8.0/KokilApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("KokilApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1f2bcd4f220d2e390e04dd3a2f3e61cf2095e92")]
 [assembly: System.Reflection.AssemblyProductAttribute("KokilApp")]
