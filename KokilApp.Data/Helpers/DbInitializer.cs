b59cd9c4a0115ae72f1c1201db7e5b1af26b705f
Nijam26 <nijam93.ctg@gmail.com>
2025-02-01 20:01:21 +0600

    After_Add_Helpers
    Helper Folder Adeded

--- KokilApp.Data/Helpers/DbInitializer.cs
+++ KokilApp.Data/Helpers/DbInitializer.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KokilApp.Data
+namespace KokilApp.Data.Helpers
 {
     public static class DbInitializer
     {
--- KokilApp/obj/Debug/net8.0/KokilApp.AssemblyInfo.cs
+++ KokilApp/obj/Debug/net8.0/KokilApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KokilApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5a3703365f43293dda1742c724c5c42d2df2b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d78abecc119dc6c4a9ae60d9277dd1434b4347")]
 [assembly: System.Reflection.AssemblyProductAttribute("KokilApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KokilApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
